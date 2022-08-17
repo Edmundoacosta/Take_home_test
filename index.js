d3ee884c902577e5e447bce2666b3007ed4c5a86
EdWorld <eacostac@cientifica.edu.pe>
2022-08-17 09:48:47 -0500

    adding endpoint for user data

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+const api = require('./routes/api');
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.use('/api', api);
 
 app.get('/', function (req, res) {
     res.send('GET request to homepage')
